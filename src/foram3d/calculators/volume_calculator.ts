38effb42fe119c6d3720f124fe907164031c667b
Bartosz Zurkowski <zurkowski.bartosz@gmail.com>
2016-01-14 02:32:20 +0100

    Reimplement total volume calculator

--- src/foram3d/calculators/volume_calculator.ts
+++ src/foram3d/calculators/volume_calculator.ts
@@ -4,21 +4,13 @@
 module Foram3D.Calculators {
   export class VolumeCalculator extends Calculator {
     calculate(): number {
-      var facesProcessor = new FacesProcessor(this.foram);
-      return facesProcessor.sumFaces(this.calculateFaceTetrahedronVolume);
-    }
+      var result = 0;
 
-    private calculateFaceTetrahedronVolume(face: Helpers.Face): number {
-      var v321, v231, v312, v132, v213, v123;
+      for (let chamber of this.foram.getActiveChambers()) {
+        result += chamber.getVolume();
+      }
 
-      v321 = face.vc.x * face.vb.y * face.va.z
-      v231 = face.vb.x * face.vc.y * face.va.z
-      v312 = face.vc.x * face.va.y * face.vb.z
-      v132 = face.va.x * face.vc.y * face.vb.z
-      v213 = face.vb.x * face.va.y * face.vc.z
-      v123 = face.va.x * face.vb.y * face.vc.z
-
-      return (-v321 + v231 +v312 - v132 - v213 + v123) / 6;
+      return result;
     }
   }
 }
